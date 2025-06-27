c6b0b583a647d2e858c083e7a1bc060a4b797569
Hubert Dykiel <hubertdykiel@Huberts-MacBook-Pro.local>
2025-06-27 23:13:17 +0200

    implement navbar

---
+++ components/NavItems.jsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { use } from 'react'
+import { cn } from '@/lib/utils'
+
+const navItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Companions', href: '/companions' },
+    { label: 'My Journey', href: '/my-journey' },
+]
+
+const NavItems = () => {
+const pathname = usePathname();
+
+  return (
+    <nav className='flex items-center gap-4'>
+        {navItems.map(({label, href}) => (
+            <Link 
+                href={href} 
+                key={label}
+                className={cn(pathname === href && 'text-primary font-semibold')}
+            >
+                {label}
+            </Link>
+        ))}
+    </nav>
+  )
+}
+
+export default NavItems
