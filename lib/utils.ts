7994c47f152f3cbe9c5c1749f1500ca1633541c0
Hubert Dykiel <hubertdykiel@Huberts-MacBook-Pro.local>
2025-07-03 22:45:19 +0200

    implement companions list

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
+import { subjectsColors } from "@/constants"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getSubjectColor = (subject: string): string => {
+  return subjectsColors[subject as keyof typeof subjectsColors]
 }
