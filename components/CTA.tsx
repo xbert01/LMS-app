b4760c8c9e7461d29ca6d021f9db53ed95a4fc7e
Hubert Dykiel <hubertdykiel@Huberts-MacBook-Pro.local>
2025-07-19 15:20:40 +0200

    implement CTA component

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className='cta-section'>
+      <div className="cta-badge">Start learning your way.</div>
+      <h2 className='text-3xl font-bold'>
+        Join our community of learners and companions today!
+      </h2>
+      <p>pick a name, subject, voice, personality - and start learning through voice </p>
+      <Image src="images/cta.svg" alt='' width={362} height={232}/>
+      <button className='btn-primary'>
+        <Image src="/icons/plus.svg" alt='plus' width={12} height={12}/>
+        <Link href="/companions/new">
+          <p>Build a New Companion</p>
+        </Link>
+      </button>
+    </section>
   )
 }
 
