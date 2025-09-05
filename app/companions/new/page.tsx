e036735071c7cc14d0b5890ac2d3c2c63cb72726
Hubert Dykiel <hubertdykiel@Huberts-MacBook-Pro.local>
2025-09-05 20:05:45 +0200

    implement Companion Form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,15 @@
+import CompanionForm from '@/components/CompanionForm'
 import React from 'react'
 
-const page = () => {
+const NewCompanion = () => {
   return (
-    <div>Companion Builder</div>
+    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
-export default page
+export default NewCompanion
